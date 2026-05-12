450ebdea36e8e067f20f7b00bc3229c9c4380a8b
Tqz <62167210+TqzDev@users.noreply.github.com>
2026-05-13 00:42:21 +0700

    Add files via upload

---
+++ api/hbz.js
@@ -0,0 +1,3 @@
+export default function handler(req, res) {
+  res.status(200).json({"status":"success","key":"*****00-00*****","max_devices":1,"used_devices":0,"created_date":"2025-12-1T00:00:00.000Z","expiry_date":"2099-12-31T00:00:00.000Z","is_registered":false});
+}
