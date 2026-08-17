1f59164d8cbcf39df011a1202e446b5210d53e91
Tqz <62167210+TqzDev@users.noreply.github.com>
2026-08-17 23:40:44 +0700

    Update ph.js

--- api/ph.js
+++ api/ph.js
@@ -1,5 +1,5 @@
-export async function GET() {
-  return Response.json({
+export default function handler(req, res) {
+  res.status(200).json({
     valid: true,
     reason: "ok",
     exp: "2099-12-31",
