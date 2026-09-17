9fad501daa2c83bd63b35eb8801ec65b4e5d1795
Tqz <62167210+TqzDev@users.noreply.github.com>
2026-09-17 13:48:09 +0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,3 +1,14 @@
 export default function handler(req, res) {
-  res.status(200).json({"status":"success","userStatus":0,"token":"hbz-mock-valid-token","refresh_token":"hbz-mock-refresh-token","expires_in":315360000,"expiry_date":"2099-12-31T23:59:59.000Z"});
-}
+  const { type } = req.query;
+
+  if (type === 'licence' || req.method === 'GET') {
+    return res.status(200).json({
+      userStatus: 0
+    });
+  }
+
+  return res.status(200).json({
+    token: "hbz-mock-valid-token-2099",
+    refresh_token: "hbz-mock-refresh-token-2099",
+    expires_in: 315360000
+  });
