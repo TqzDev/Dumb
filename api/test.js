ce77ab48687633fc1644a58897e8799ecd990434
Tqz <62167210+TqzDev@users.noreply.github.com>
2026-09-17 13:52:34 +0700

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,18 +1,10 @@
 module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
   res.status(200).json({
     status: "success",
-    key: "*****00-00*****",
-    max_devices: 1,
-    used_devices: 0,
-    created_date: "2025-12-01T00:00:00.000Z",
-    expiry_date: "2099-12-31T23:59:59.000Z",
-    is_registered: false,
     userStatus: 0,
-    token: "hbz-mock-valid-token-2099",
-    refresh_token: "hbz-mock-refresh-token-2099",
-    expires_in: 315360000
+    token: "hbz-mock-valid-token",
+    refresh_token: "hbz-mock-refresh-token",
+    expires_in: 315360000,
+    expiry_date: "2099-12-31T23:59:59.000Z"
   });
 };
